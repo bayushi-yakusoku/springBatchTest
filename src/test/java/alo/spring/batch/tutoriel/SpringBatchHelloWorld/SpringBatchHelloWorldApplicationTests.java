a9ead383cd4aac2c6ec4c2aa45ff07c244585d6e
Papa <bayushi.tonio@gmail.com>
2020-09-01 12:05:26 +0200

    Initialisation

---
+++ src/test/java/alo/spring/batch/tutoriel/SpringBatchHelloWorld/SpringBatchHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package alo.spring.batch.tutoriel.SpringBatchHelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
