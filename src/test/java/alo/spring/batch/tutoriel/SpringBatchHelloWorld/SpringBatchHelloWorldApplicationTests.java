9770f0a5cecdec9a73a71da8606a31bb7541530b
Papa <bayushi.tonio@gmail.com>
2021-01-20 09:12:25 +0100

    Parameters update for Tests (contextLoads)

--- src/test/java/alo/spring/batch/tutoriel/SpringBatchHelloWorld/SpringBatchHelloWorldApplicationTests.java
+++ src/test/java/alo/spring/batch/tutoriel/SpringBatchHelloWorld/SpringBatchHelloWorldApplicationTests.java
@@ -3,7 +3,7 @@ package alo.spring.batch.tutoriel.SpringBatchHelloWorld;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest (args = {"fileName=pouf.csv", "name=pouf"})
 class SpringBatchHelloWorldApplicationTests {
 
 	@Test
