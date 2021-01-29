1ec70b040c8e0595cab7d8e9aad454b13413236b
Papa <bayushi.tonio@gmail.com>
2021-01-29 17:00:00 +0100

    Working with Windows input file
    Work with those parameters (played with /\ for testing):
    inputFile=file:F:\Code\Items\inputFile.csv
    outputFile=file:F:/Code/Items/outputFile.csv

--- src/main/java/alo/spring/batch/tutoriel/SpringBatchHelloWorld/FirstChunkJob.java
+++ src/main/java/alo/spring/batch/tutoriel/SpringBatchHelloWorld/FirstChunkJob.java
@@ -4,14 +4,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.batch.item.file.mapping.PassThroughLineMapper;
+import org.springframework.batch.item.file.transform.PassThroughLineAggregator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 @Configuration
 public class FirstChunkJob {
@@ -23,18 +31,45 @@ public class FirstChunkJob {
     @Autowired
     private StepBuilderFactory stepBuilderFactory;
 
-//    @Bean
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    public DefaultJobParametersValidator parameterValidator() {
+        return new DefaultJobParametersValidator(
+                new String[]{"inputFile", "outputFile"},
+                new String[]{"run.time", "fileName", "executionDate"});
+    }
+
+    @Bean
+    @StepScope
+    public FlatFileItemReader<String> itemReader(@Value("#{jobParameters['inputFile']}") Resource inputFile) {
+        logger.info("inputFile: " + inputFile);
+
+        return new FlatFileItemReaderBuilder<String>()
+                .name("itemReader")
+                .resource(inputFile)
+                .lineMapper(new PassThroughLineMapper())
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public FlatFileItemWriter<String> itemWriter(@Value("#{jobParameters['outputFile']}") Resource outputFile) {
+        logger.info("outputFile: " + outputFile.getFilename());
+
+        return new FlatFileItemWriterBuilder<String>()
+                .name("itemWriter")
+                .resource(outputFile)
+                .lineAggregator(new PassThroughLineAggregator<>())
+                .build();
+    }
+
     public Step stepReadFile() {
         return stepBuilderFactory
                 .get("Step read file")
-                .tasklet((contribution, chunkContext) -> {
-                    String fileName = "";
-                    fileName = (String) chunkContext.getStepContext().getJobParameters().get("fileName");
-
-                    logger.info("Processing file: " + fileName);
-
-                    return RepeatStatus.FINISHED;
-                })
+                .<String, String>chunk(10)
+                .reader(itemReader(null))
+                .writer(itemWriter(null))
                 .build();
     }
 
@@ -43,6 +78,7 @@ public class FirstChunkJob {
         return jobBuilderFactory
                 .get("Job Read file")
                 .start(stepReadFile())
+                .validator(parameterValidator())
                 .incrementer(new ParameterAddRunTime())
                 .build();
     }
